0dd1032b077a01bee764e7f1df93f15e7602ec85
Lee <helenslee1@gmail.com>
2024-04-22 13:56:41 -0400

    adding personal StringArray demo project

---
+++ Week2/StringArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace StringArray;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine ("Hello");
+        
+        string[] groceryList = new string[5];
+
+        Console.WriteLine("Lets make a grocery list");  
+
+        for (int i = 0; i < groceryList.Length; i++)
+        {
+            Console.WriteLine($"Enter item {i + 1}: ");
+            groceryList[i] = Console.ReadLine();
+        }
+       
+       //Console.WriteLine ("This is your grocery list so far:");
+
+    
+    }
+}
